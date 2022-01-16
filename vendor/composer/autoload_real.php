81cb4cbd8e1a99ccea00478579f42527dfcdde40
bert-fabillar <>
2022-01-16 20:44:32 +0800

    composer update and vendor libraries

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitec5a9a1ef431846a44bd9e6bd2442a79
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitec5a9a1ef431846a44bd9e6bd2442a79', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitec5a9a1ef431846a44bd9e6bd2442a79', 'loadClassLoader'));
