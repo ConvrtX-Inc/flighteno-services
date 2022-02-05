391716130842b935faa2b99f925d69148dbe6365
bert-convrtx <90181731+bert-convrtx@users.noreply.github.com>
2022-02-05 12:17:02 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://localhost/flighteno-services/';
-$config['base_url'] = 'https://localhost:443/flighteno-services';
+$config['base_url'] = 'https://dev-flighteno-convrtx.herokuapp.com/';
 //$config['base_url'] = 'https://dev-flighteno-convrtx.herokuapp.com/';
 //$config['base_url'] = 'http://3.124.117.144/flighteno-services';
 // $config['base_url'] = 'http://localhost/flighteno';
