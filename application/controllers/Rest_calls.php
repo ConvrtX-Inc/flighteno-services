f8f48d25a035f76b6a5b8755df7fe2048028b6b9
bert-fabillar <>
2022-02-09 16:10:39 +0800

    Order Notification History

--- application/controllers/Rest_calls.php
+++ application/controllers/Rest_calls.php
@@ -2407,7 +2407,7 @@ class Rest_calls extends REST_Controller
                 $response_array = [
                     'data' => $orderData,
                     'profile' => $userProfileData,
-                    'order_history' => orderHistory,
+                    'order_history' => $orderHistory,
                     'status' => 'Successfully Fetched!',
                 ];
                 $this->set_response($response_array, REST_Controller::HTTP_CREATED);
