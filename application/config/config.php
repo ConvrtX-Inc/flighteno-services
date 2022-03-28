040ac72429d1a29c147c351a69b4f4a801bab328
jestoni-convrtx <jestonisalonga@convrtx.com>
2022-03-28 15:14:20 +0800

    Updated base url

--- application/config/config.php
+++ application/config/config.php
@@ -24,11 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://localhost/flighteno-services/';
-$config['base_url'] = 'https://dev-flighteno-convrtx.herokuapp.com/';
-//$config['base_url'] = 'https://dev-flighteno-convrtx.herokuapp.com/';
-//$config['base_url'] = 'http://3.124.117.144/flighteno-services';
-// $config['base_url'] = 'http://localhost/flighteno';
-// $config['base_url'] = 'http://flighteno.techticksdigital.com';
+$config['base_url'] = 'https://flighteno-dev.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
