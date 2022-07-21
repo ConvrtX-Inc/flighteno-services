6a7b354ababf9ba51d9b59009f1c88202e0e08f2
Jestoni <jestonisalonga@convrtx.com>
2022-07-21 13:45:37 +0800

    FLIGHT-123: Update Twilio tokens and Service ID

--- application/config/config.php
+++ application/config/config.php
@@ -535,5 +535,5 @@ $config['service_id'] = 'VAa0a01310d8c2116de1581afe34d6ba87';
  * service_id = app service id
  */
 $config['sid'] = 'AC81e89cf6e91eb32d34fb5c700ff5fbe0';
-$config['twilio_token'] = 'd006d71818e794b255c488d5801586b3';
-$config['service_id'] = 'VA6d54c252ce05404db16fc3cbdecd53e2';
+$config['twilio_token'] = '5c34923aa392ca780de363a89d83f064';
+$config['service_id'] = 'MGd0f4b0986bf2391914c8526f05cf7661';
